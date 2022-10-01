68eeb84dcb3067474c0cdd4e75e672a8f8967636
Awoked <alper.kossay@gmail.com>
2022-10-01 20:05:32 +0300

    v12

--- js/firebase/firebase.js
+++ js/firebase/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyC9TcKYgK-mLtEYsckJ3RQZuCxR-YPWKmQ",
     authDomain: "koalla-6916a.firebaseapp.com",
@@ -16,7 +12,6 @@ const firebaseConfig = {
     measurementId: "G-VR8PZF596W"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 
@@ -38,20 +33,24 @@ function insertData() {
     var enterName = document.querySelector('#enterMovieName');
     var enterRate = document.querySelector('#enterMovieRate');
     var enterTime = document.querySelector('#enterMovieTime');
-    set(ref(db, "movies/" + enterID.value), {
-        name : enterName.value,
-        ID : enterID.value,
-        date : enterDate.value,
-        details : enterDetails.value,
-        image : enterImg.value,
-        link : enterLink.value,
-        rate : enterRate.value,
-        time : enterTime.value
-    }).then(()=>{
-        $('.database-success').text("Veri Başarıyla Eklendi");
-    }).catch((error)=>{
-        alert(error);
-    });
+    if (enterID.value == "") {
+        alert("ID girmeniz zorunludur");
+    } else {
+        set(ref(db, "movies/" + enterID.value), {
+            name : enterName.value,
+            ID : enterID.value,
+            date : enterDate.value,
+            details : enterDetails.value,
+            image : enterImg.value,
+            link : enterLink.value,
+            rate : enterRate.value,
+            time : enterTime.value
+        }).then(()=>{
+            $('.database-success').removeClass('text-danger').addClass('text-success').text("Veri Başarıyla Eklendi");
+        }).catch((error)=>{
+            alert(error);
+        });
+    }
 }
 
 
@@ -67,33 +66,42 @@ function UpdateData() {
     var enterName = document.querySelector('#enterMovieName');
     var enterRate = document.querySelector('#enterMovieRate');
     var enterTime = document.querySelector('#enterMovieTime');
-    update(ref(db, "movies/" + enterID.value),{
-        name : enterName.value,
-        ID : enterID.value,
-        date : enterDate.value,
-        details : enterDetails.value,
-        image : enterImg.value,
-        link : enterLink.value,
-        rate : enterRate.value,
-        time : enterTime.value
-    })
-    .then(()=>{
-        $('.database-success').text("Veri Başarıyla Güncellendi");
-    })
-    .catch((error)=>{
-        alert(error)
-    });
+    if (enterID.value == "") {
+        alert("ID girmeniz zorunludur");
+    } else{
+        update(ref(db, "movies/" + enterID.value),{
+            name : enterName.value,
+            ID : enterID.value,
+            date : enterDate.value,
+            details : enterDetails.value,
+            image : enterImg.value,
+            link : enterLink.value,
+            rate : enterRate.value,
+            time : enterTime.value
+        })
+        .then(()=>{
+            $('.database-success').removeClass('text-danger').addClass('text-success').text("Veri Başarıyla Güncellendi");
+        })
+        .catch((error)=>{
+            alert(error)
+        });
+    }
 }
 
 function RemoveData() {
     var enterID = document.querySelector('#enterID');
-    remove(ref(db, "movies/" + enterID.value))
-    .then(()=>{
-        $('.database-success').text("Veri Başarıyla Silindi");
-    })
-    .catch((error)=>{
-        alert(error);
-    })
+    console.log(enterID.value);
+    if (enterID.value == "") {
+        alert("ID Girmeniz Zorunludur");
+    } else {
+        remove(ref(db, "movies/" + enterID.value))
+        .then(()=>{
+            $('.database-success').removeClass('text-success').addClass('text-danger').text("Veri Başarıyla Silindi");
+        })
+        .catch((error)=>{
+            alert(error);
+        })
+    }
 }
 
 
@@ -107,26 +115,89 @@ function getAllDataOnce() {
         snapshot.forEach(childSnapshot => {
             movies.push(childSnapshot.val());
         });
-        movies.forEach(element => {
+        movies.forEach(movie => {
+            // Filmler Kısmı Render
             $('.new-movies').append(`
-            <div class="movie-item" data-id="${element.ID}">
+            <div class="movie-item" data-id="${movie.ID}">
                 <a href="#" class="text-decoration-none">
                     <div class="movie-item-img">
-                        <img src="${element.image}" class="img-fluid">
+                        <img src="${movie.image}" class="img-fluid">
                     </div>
                     <div class="movie-item-title p-2 px-3">
                         <h6 class="text-light text-truncate" id="movie-name">
-                            ${element.name}
+                            ${movie.name}
                         </h6>
                         <div class="d-flex gap-2 text-truncate">
-                            <span id="movie-date">${element.date}</span>
-                            <span id="movie-time" class="text-truncate">${element.time} - dakika</span>
+                            <span id="movie-date">${movie.date}</span>
+                            <span id="movie-time" class="text-truncate">${movie.time} - dakika</span>
                         </div>
                     </div>
                 </a>
             </div>
-        `);
+            `);
+
+            // Slider Kısmı Render
+            // $('.carousel-inner').append(`
+            //     <div class="carousel-item" style="background: url(${movie.image}); background-repeat: no-repeat; background-size: 100%; 
+            //     background-position: center;">
+            //     <div class="slider-item d-flex flex-column flex-md-row justify-content-md-center align-items-center gap-3 gap-md-0 py-3 px-5">
+            //         <div class="col-12 col-md-4 col-xxl-5 justify-content-center d-flex justify-content-md-end ps-md-4">
+            //             <img src="${movie.image}" id="slider-item-image" class="img-fluid rounded">
+            //         </div>
+            //         <div class="col-12 col-md-8 pe-md-5 col-xxl-7 px-md-4 justify-content-center justify-content-md-center  d-flex gap-md-2 gap-1 flex-column">
+            //             <div class="d-flex">
+            //                 <h3 class="text-light text-truncate" id="movie-title">${movie.name}</h3>
+            //             </div>
+            //             <div class="d-flex gap-3">
+            //                 <span class="text-light text-truncate" id="movie-year">${movie.date}</span>
+            //                 <span class="text-light text-truncate" id="movie-time">${movie.time} - dakika</span>
+            //                 <span class="text-light text-truncate" id="movie-rate">Imdb ${movie.rate}</span>
+            //             </div>
+            //             <div class="d-flex">
+            //                 <p id="movie-subtitle">
+            //                     ${movie.details}
+            //                 </p>
+            //             </div>
+            //             <div class="watch-button-wrapper d-flex ">
+            //                 <button class="btn sliderButtonWatch rounded-pill px-3 d-flex text-truncate gap-2 align-items-center" data-slider-id="${movie.ID}"><i class="fa-solid fa-play"></i><span>Şimdi İzle</span></button>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     </div>
+            // `);
+            // $('.carousel-item:first').addClass('active');
         });
+        for (let i = 0; i < 6; i++) {
+            $('.carousel-inner').append(`
+            <div class="carousel-item" style="background: url(${movies[i].image}); background-repeat: no-repeat; background-size: 100%; 
+            background-position: center;">
+            <div class="slider-item d-flex flex-column flex-md-row justify-content-md-center align-items-center gap-3 gap-md-0 py-3 px-5">
+                <div class="col-12 col-md-4 col-xxl-5 justify-content-center d-flex justify-content-md-end ps-md-4">
+                    <img src="${movies[i].image}" id="slider-item-image" class="img-fluid rounded">
+                </div>
+                <div class="col-12 col-md-8 pe-md-5 col-xxl-7 px-md-4 justify-content-center justify-content-md-center  d-flex gap-md-2 gap-1 flex-column">
+                    <div class="d-flex">
+                        <h3 class="text-light text-truncate" id="movie-title">${movies[i].name}</h3>
+                    </div>
+                    <div class="d-flex gap-3">
+                        <span class="text-light text-truncate" id="movie-year">${movies[i].date}</span>
+                        <span class="text-light text-truncate" id="movie-time">${movies[i].time} - dakika</span>
+                        <span class="text-light text-truncate" id="movie-rate">Imdb ${movies[i].rate}</span>
+                    </div>
+                    <div class="d-flex">
+                        <p id="movie-subtitle">
+                            ${movies[i].details}
+                        </p>
+                    </div>
+                    <div class="watch-button-wrapper d-flex ">
+                        <button class="btn sliderButtonWatch rounded-pill px-3 d-flex text-truncate gap-2 align-items-center" data-slider-id="${movies[i].ID}"><i class="fa-solid fa-play"></i><span>Şimdi İzle</span></button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        `);
+        $('.carousel-item:first').addClass('active');
+        }
     })
 }
 
@@ -158,7 +229,6 @@ let customMovieHide = () =>{
 
 $(document).on('click', '.movie-item',function () {
     let data_Id = this.getAttribute('data-id');
-    console.log(data_Id);
     const dbRef = ref(db);
     get(child(dbRef, "movies"))
     .then((snapshot)=>{
@@ -169,15 +239,32 @@ $(document).on('click', '.movie-item',function () {
         movies.forEach(movieItem =>{
             if (data_Id == movieItem.ID) {
                 customMovieShow();
-                console.log(movieItem.link);
                 $('#movie-player').attr('src', movieItem.link);
             }
         })
     });
-    
+});
+
+$(document).on('click', '.sliderButtonWatch',function () {
+    let data_slider_Id = this.getAttribute('data-slider-id');
+    const dbRef = ref(db);
+    get(child(dbRef, "movies"))
+    .then((snapshot)=>{
+        let movies = [];
+        snapshot.forEach(childSnapshot =>{
+            movies.push(childSnapshot.val());
+        });
+        movies.forEach(movieItem =>{
+            if (data_slider_Id == movieItem.ID) {
+                customMovieShow();
+                $('#movie-player').attr('src', movieItem.link);
+            }
+        })
+    });
 });
 
 $('#movie-close').click(function () { 
     customMovieHide();
     $('#movie-player').removeAttr('src');
 });
+
